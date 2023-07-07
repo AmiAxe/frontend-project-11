8b38fbc592a032f3540411c6c70fdbcabff09215
Maksim Angelov <almostparagon@gmail.com>
2023-07-07 22:02:17 +0500

    Add files via upload

---
+++ src/locales/resources.js
@@ -0,0 +1,14 @@
+export default {
+  ru: {
+    translation: {
+      feeds: 'Фиды',
+      posts: 'Посты',
+      browse: 'Просмотр',
+      success: 'RSS успешно загружен',
+      notOneOf: 'RSS уже существует',
+      notValid: 'Ссылка должна быть валидным URL',
+      fall: 'Ресурс не содержит валидный RSS',
+      networkError: 'Ошибка сети',
+    },
+  },
+};
