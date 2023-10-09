b7de618a3d350b91038707f8d218c443b8996af5
Maksim Angelov <almostparagon@gmail.com>
2023-10-09 15:22:16 +0500

    Update application.js

--- src/application.js
+++ src/application.js
@@ -133,11 +133,9 @@ const app = () => {
 
       elements.containerPosts.addEventListener('click', (e) => {
         const currentId = e.target.dataset.id;
-        if (currentId) {
-          watchedState.ui.currentPost = currentId;
-          if (!watchedState.ui.openedPostsId.includes(currentId)) {
-            watchedState.ui.openedPostsId = [currentId, ...watchedState.ui.openedPostsId];
-          }
+        watchedState.ui.currentPost = currentId;
+        if (!watchedState.ui.openedPostsId.includes(currentId)) {
+          watchedState.ui.openedPostsId = [currentId, ...watchedState.ui.openedPostsId];
         }
       });
 
